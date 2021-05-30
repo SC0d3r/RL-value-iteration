521f48401a821f34e65e9ba4e85c9c360ea7dfeb
SC0d3r <sasan.kelishani@gmail.com>
2021-05-31 00:46:27 +0430

    use GridTable and GridDirectionTable to show value iteration after each step

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { addBlock, addTerminalState, algorithm, makeGrid } from "./logic";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import GridTable from "./GridTable";
+import GridDirectionTable from "./GridDirectionTable";
 function App() {
+  let defaultgrid = makeGrid(3, 4);
+  defaultgrid = addBlock(1, 1, defaultgrid);
+  defaultgrid = addTerminalState(0, 3, 1, defaultgrid);
+  defaultgrid = addTerminalState(1, 3, -1, defaultgrid);
+
+  const [R, setR] = useState(0);
+  const [gamma, setgamma] = useState(0.9);
+  const [step, setStep] = useState(1);
+  const [grid, setGrid] = useState(defaultgrid);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography
+            component="h1"
+            style={{
+              padding: "2rem",
+              textAlign: "center",
+              fontSize: "calc(1.4rem + 12px)",
+            }}
+          >
+            RL Value Iteration
+          </Typography>
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          alignItems="center"
+          justify="center"
+          direction="row"
         >
-          Learn React
-        </a>
-      </header>
+          <Grid
+            item
+            xs={2}
+            container
+            direction="row"
+            alignItems="center"
+            justify="center"
+          >
+            <Grid item style={{ padding: "4px" }}>
+              r ={" "}
+            </Grid>
+            <Grid item>
+              <TextField
+                onChange={(e) => {
+                  setStep(1);
+                  setGrid(defaultgrid);
+                  // const x = +e.target.value;
+                  // setR(isNaN(x) ? 0 : x);
+                  setR(e.target.value);
+                }}
+                style={{ width: "80px" }}
+                variant="outlined"
+                margin="dense"
+                // type="number"
+                value={R}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            xs={2}
+            container
+            direction="row"
+            alignItems="center"
+            justify="center"
+          >
+            <Grid item style={{ padding: "4px" }}>
+              &gamma; ={" "}
+            </Grid>
+            <Grid item>
+              <TextField
+                onChange={(e) => {
+                  setgamma((x) => Math.max(0, Math.min(1, +e.target.value)));
+                  setStep(1);
+                  setGrid(defaultgrid);
+                }}
+                style={{ width: "80px" }}
+                variant="outlined"
+                margin="dense"
+                type="number"
+                value={gamma}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          // alignItems="center"
+          style={{
+            minHeight: "55vh",
+          }}
+        >
+          <Grid item xs={12} md={6} sm={6} style={{}}>
+            <GridTable justify="flex-end" grid={grid} />
+          </Grid>
+          <Grid item xs={12} md={6} sm={6}>
+            <GridDirectionTable grid={grid} />
+          </Grid>
+        </Grid>
+
+        <Grid item container alignItems="center" justify="center" spacing={2}>
+          <Grid item>
+            <TextField
+              type="number"
+              value={step}
+              onChange={(e) => {
+                setStep(+e.target.value);
+                setGrid(defaultgrid);
+              }}
+              variant="outlined"
+              margin="dense"
+              style={{ width: "80px", margin: "auto" }}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              style={{ height: "39px" }}
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                const reward = isNaN(+R) ? 0 : +R;
+                if (isNaN(+R)) alert("Reward is not a valid number");
+
+                setGrid(algorithm(defaultgrid, step, reward, gamma));
+                setStep((x) => x + 1);
+              }}
+            >
+              step
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
